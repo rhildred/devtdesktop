e52b6373693c8a30d21e6dfc56a681ad997d6c3b
Richard Hildred <rhildred@gmail.com>
2018-10-27 18:19:06 -0400

    added my timezone

--- toaster.sh
+++ toaster.sh
@@ -14,3 +14,4 @@ sudo usermod -aG docker $1
 sudo apt install gdebi-core
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi google-chrome-stable_current_amd64.deb
+sudo timedatectl set-timezone America/Toronto
