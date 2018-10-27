ca03e85b031732c800e037ae7f8dbdcb6c9b79ed
Richard Hildred <rhildred@gmail.com>
2018-10-27 17:08:23 -0400

    Update toaster.sh

--- toaster.sh
+++ toaster.sh
@@ -7,9 +7,9 @@ sudo apt-get install docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-adduser $1
-usermod -aG sudo $1
-usermod -aG docker $1
+sudo adduser $1
+sudo usermod -aG sudo $1
+sudo usermod -aG docker $1
 #chrome
 sudo apt install gdebi-core
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
